81ea5ff84260bda1ed6ac9031b977d4d5898b9ce
Leonardo Vitor Bassi de Lima <leonardobassi@Leonardos-MacBook-Air.local>
2025-12-16 22:28:53 -0300

    feat: Created a Spec to test the Test Cases page

--- cypress/support/pages/homePage.ts
+++ cypress/support/pages/homePage.ts
@@ -4,6 +4,11 @@ export class HomePage {
     cy.contains('Features Items').should('be.visible')
   }
 
+  verifyUserIsLoggedIn() {
+    cy.contains('Logged in as').should('be.visible')
+    cy.log('User is logged in')
+}
+
   goToLoginPage() {
     cy.get('i[class="fa fa-lock"]').click()
   }
@@ -11,6 +16,10 @@ export class HomePage {
   goToContactUsPage() {
     cy.get('a[href="/contact_us"]').click()
     cy.contains('Contact Us').should('be.visible')
+  }
+
+  goToTestCasesPage() {
+    cy.get('a[href="/test_cases"]').first().click()
   }
 
   logout() {
