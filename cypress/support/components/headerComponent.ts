75bb57e40b02c68ae755e7072b64eb9626d58c0d
Le0bass1 <leo.lima.leo@outlook.com>
2025-12-19 12:45:07 -0300

    Refactor: The header component has been organized.

--- cypress/support/components/headerComponent.ts
+++ cypress/support/components/headerComponent.ts
@@ -1,36 +1,46 @@
 export class HeaderComponent {
-    goToHome() {
-      cy.get('a[href="/"]').contains('Home').click()
-    }
-  
-    goToProducts() {
-      cy.get('a[href="/products"]').click()
-    }
-  
-    goToCart() {
-      cy.get('a[href="/view_cart"]').click()
-    }
-  
-    goToLoginPage() {
-      cy.get('i[class="fa fa-lock"]').click()
-    }
-  
-    goToTestCasesPage() {
-      cy.get('a[href="/test_cases"]').first().click()
-    }
-  
-    goToContactUsPage() {
-      cy.get('a[href="/contact_us"]').click()
-    }
-  
-    logout() {
-      cy.get('a[href="/logout"]').click()
-    }
-  
-    verifyUserIsLoggedIn(username?: string) {
-      cy.contains('Logged in as').should('be.visible')
-      if (username) {
-        cy.contains(`Logged in as ${username}`).should('be.visible')
-      }
+  selectors = {
+    homeLink: 'a[href="/"]',
+    productsLink: 'a[href="/products"]',
+    cartLink: 'a[href="/view_cart"]',
+    loginIcon: 'i[class="fa fa-lock"]',
+    testCasesLink: 'a[href="/test_cases"]',
+    contactUsLink: 'a[href="/contact_us"]',
+    logoutLink: 'a[href="/logout"]',
+  };
+
+  goToHome() {
+    cy.get(this.selectors.homeLink).click()
+  }
+
+  goToProducts() {
+    cy.get(this.selectors.productsLink).click()
+  }
+
+  goToCart() {
+    cy.get(this.selectors.cartLink).click()
+  }
+
+  goToLoginPage() {
+    cy.get(this.selectors.loginIcon).click()
+  }
+
+  goToTestCasesPage() {
+    cy.get(this.selectors.testCasesLink).first().click()
+  }
+
+  goToContactUsPage() {
+    cy.get(this.selectors.contactUsLink).click()
+  }
+
+  logout() {
+    cy.get(this.selectors.logoutLink).click()
+  }
+
+  verifyUserIsLoggedIn(username?: string) {
+    cy.contains('Logged in as').should('be.visible')
+    if (username) {
+      cy.contains(`Logged in as ${username}`).should('be.visible')
     }
   }
+}
